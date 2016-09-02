3f86df760e9ec10d8dfea2bcab0a64ac1afa6b30
Joe Turner <joe@oampo.co.uk>
2016-09-02 13:59:26 +0100

    Add split out runServer function.

--- server/index.js
+++ server/index.js
@@ -1,6 +1,29 @@
+import 'babel-polyfill';
 import express from 'express';
 
+const HOST = process.env.HOST;
+const PORT = process.env.PORT || 8080;
+
+console.log(`Server running in ${process.env.NODE_ENV} mode`);
+
 let app = express();
+
 app.use(express.static(process.env.CLIENT_PATH));
 
-app.listen(process.env.PORT || 8080);
+function runServer() {
+    return new Promise((resolve, reject) => {
+        app.listen(PORT, HOST, (err) => {
+            if (err) {
+                console.error(err);
+                reject(err);
+            }
+
+            const host = HOST || 'localhost';
+            console.log(`Listening on ${host}:${PORT}`);
+        });
+    });
+}
+
+if (require.main === module) {
+    runServer();
+}
